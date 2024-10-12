2453a8002ff89a396be07d5cf3bda9e961067366
Alex Gaetano Padula <apadula@cursusdb.com>
2024-10-12 16:11:39 -0400

    test corrections

--- lsmt_test.go
+++ lsmt_test.go
@@ -50,8 +50,8 @@ func TestLMST_Put(t *testing.T) {
 		t.Fatal("expected non-nil lmst")
 	}
 
-	// Insert 256 key-value pairs
-	for i := 0; i < 256; i++ {
+	// Insert 268 key-value pairs
+	for i := 0; i < 268; i++ {
 		err = lsmt.Put([]byte(string(fmt.Sprintf("%d", i))), []byte(string(fmt.Sprintf("%d", i))))
 		if err != nil {
 			t.Fatal(err)
@@ -82,7 +82,7 @@ func TestLMST_Put(t *testing.T) {
 
 func TestLMST_Compact(t *testing.T) {
 	defer os.RemoveAll("my_lsm_tree")
-	lsmt, err := New("my_lsm_tree", 0755, 128, 2, 1)
+	lsmt, err := New("my_lsm_tree", 0755, 128, 3, 1)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -99,12 +99,8 @@ func TestLMST_Compact(t *testing.T) {
 		}
 	}
 
-	// this will create 3 sstables, the system will know at that point as sstables is over compactionInverval of 2 at that point will compact
-	// 0.sst, 1.sst, 2.sst
-	// to 0.sst
-
-	if len(lsmt.sstables) != 1 {
-		t.Fatalf("expected 1 sstables, got %d", len(lsmt.sstables))
+	if len(lsmt.sstables) != 2 {
+		t.Fatalf("expected 2 sstables, got %d", len(lsmt.sstables))
 	}
 
 	// Check for 0.sst
@@ -143,11 +139,11 @@ func TestLMST_Delete(t *testing.T) {
 
 	// There should be 2 sstables
 	// 0.sst and 1.sst
-	if len(lsmt.sstables) != 2 {
-		t.Fatalf("expected 2 sstables, got %d", len(lsmt.sstables))
+	if len(lsmt.sstables) != 1 {
+		t.Fatalf("expected 1 sstables, got %d", len(lsmt.sstables))
 	}
 
-	expectFiles := []string{"0.sst", "1.sst"}
+	expectFiles := []string{"0.sst"}
 
 	// Read the directory
 	files, err := os.ReadDir("my_lsm_tree")
@@ -349,4 +345,200 @@ func TestLSMT_NRange(t *testing.T) {
 		}
 	}
 
+}
+
+func TestLSMT_GreaterThan(t *testing.T) {
+	defer os.RemoveAll("my_lsm_tree")
+	lsmt, err := New("my_lsm_tree", 0755, 128, 2, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if lsmt == nil {
+		t.Fatal("expected non-nil lmst")
+	}
+
+	for i := 0; i < 10; i++ {
+		err = lsmt.Put([]byte(string(fmt.Sprintf("%d", i))), []byte(string(fmt.Sprintf("%d", i))))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	keys, _, err := lsmt.GreaterThan([]byte("4"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(keys) != 5 {
+		t.Fatalf("expected 5 keys, got %d", len(keys))
+	}
+
+	expectKeys := [][]byte{
+		[]byte("5"),
+		[]byte("6"),
+		[]byte("7"),
+		[]byte("8"),
+		[]byte("9"),
+	}
+
+	for _, key := range keys {
+		for j, expectKey := range expectKeys {
+			if string(key) == string(expectKey) {
+				// remove the key from the expectKeys
+				expectKeys = append(expectKeys[:j], expectKeys[j+1:]...)
+				break
+			}
+			if j == len(expectKeys)-1 {
+				t.Fatalf("expected key to be %s, got %s", string(expectKey), string(key))
+			}
+		}
+	}
+}
+
+func TestLSMT_LessThan(t *testing.T) {
+	defer os.RemoveAll("my_lsm_tree")
+	lsmt, err := New("my_lsm_tree", 0755, 128, 2, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if lsmt == nil {
+		t.Fatal("expected non-nil lmst")
+	}
+
+	for i := 0; i < 10; i++ {
+		err = lsmt.Put([]byte(string(fmt.Sprintf("%d", i))), []byte(string(fmt.Sprintf("%d", i))))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	keys, _, err := lsmt.LessThan([]byte("4"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(keys) != 4 {
+		t.Fatalf("expected 4 keys, got %d", len(keys))
+	}
+
+	expectKeys := [][]byte{
+		[]byte("0"),
+		[]byte("1"),
+		[]byte("2"),
+		[]byte("3"),
+	}
+
+	for _, key := range keys {
+		for j, expectKey := range expectKeys {
+			if string(key) == string(expectKey) {
+				// remove the key from the expectKeys
+				expectKeys = append(expectKeys[:j], expectKeys[j+1:]...)
+				break
+			}
+			if j == len(expectKeys)-1 {
+				t.Fatalf("expected key to be %s, got %s", string(expectKey), string(key))
+			}
+		}
+	}
+}
+
+func TestLSMT_GreaterThanEqual(t *testing.T) {
+	defer os.RemoveAll("my_lsm_tree")
+	lsmt, err := New("my_lsm_tree", 0755, 128, 2, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if lsmt == nil {
+		t.Fatal("expected non-nil lmst")
+	}
+
+	for i := 0; i < 10; i++ {
+		err = lsmt.Put([]byte(string(fmt.Sprintf("%d", i))), []byte(string(fmt.Sprintf("%d", i))))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	keys, _, err := lsmt.GreaterThanEqual([]byte("4"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(keys) != 6 {
+		t.Fatalf("expected 6 keys, got %d", len(keys))
+	}
+
+	expectKeys := [][]byte{
+		[]byte("4"),
+		[]byte("5"),
+		[]byte("6"),
+		[]byte("7"),
+		[]byte("8"),
+		[]byte("9"),
+	}
+
+	for _, key := range keys {
+		for j, expectKey := range expectKeys {
+			if string(key) == string(expectKey) {
+				// remove the key from the expectKeys
+				expectKeys = append(expectKeys[:j], expectKeys[j+1:]...)
+				break
+			}
+			if j == len(expectKeys)-1 {
+				t.Fatalf("expected key to be %s, got %s", string(expectKey), string(key))
+			}
+		}
+	}
+}
+
+func TestLSMT_LessThanEqual(t *testing.T) {
+	defer os.RemoveAll("my_lsm_tree")
+	lsmt, err := New("my_lsm_tree", 0755, 128, 2, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if lsmt == nil {
+		t.Fatal("expected non-nil lmst")
+	}
+
+	for i := 0; i < 10; i++ {
+		err = lsmt.Put([]byte(string(fmt.Sprintf("%d", i))), []byte(string(fmt.Sprintf("%d", i))))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	keys, _, err := lsmt.LessThanEqual([]byte("4"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(keys) != 5 {
+		t.Fatalf("expected 5 keys, got %d", len(keys))
+	}
+
+	expectKeys := [][]byte{
+		[]byte("0"),
+		[]byte("1"),
+		[]byte("2"),
+		[]byte("3"),
+		[]byte("4"),
+	}
+
+	for _, key := range keys {
+		for j, expectKey := range expectKeys {
+			if string(key) == string(expectKey) {
+				// remove the key from the expectKeys
+				expectKeys = append(expectKeys[:j], expectKeys[j+1:]...)
+				break
+			}
+			if j == len(expectKeys)-1 {
+				t.Fatalf("expected key to be %s, got %s", string(expectKey), string(key))
+			}
+		}
+	}
 }
