83b05a551c1f99d492bff530e9c7778a2150f2e6
Alex Gaetano Padula <apadula@cursusdb.com>
2024-10-13 01:37:06 -0400

    transaction implementation, optimized read from sstables to iterate instead of reading all into memory which can cost lots of memory, write ahead log implementation, recovery

--- lsmt.go
+++ lsmt.go
@@ -20,7 +20,7 @@ import (
 	"encoding/gob"
 	"errors"
 	"fmt"
-	"log"
+	"io"
 	"lsmt/avl"
 	"os"
 	"strings"
@@ -30,26 +30,65 @@ import (
 
 const SSTABLE_EXTENSION = ".sst"
 const TOMBSTONE_VALUE = "$tombstone"
+const WAL_EXTENSION = ".wal"
 
 // LSMT is the main struct for the log-structured merge-tree.
 type LSMT struct {
-	memtable           *avl.AVLTree  // The memtable is an in-memory AVL tree.
-	memtableSize       atomic.Int64  // The size of the memtable.
-	memtableLock       *sync.RWMutex // Lock for the memtable.
-	sstables           []*SSTable    // The list of current SSTables.
-	sstablesLock       *sync.RWMutex // Lock for the list of SSTables.
-	directory          string        // The directory where the SSTables are stored.
-	memtableFlushSize  int           // The size at which the memtable should be flushed to disk.
-	compactionInterval int           // The interval at which the LSM-tree should be compacted. (in number of SSTables)
-	minimumSSTables    int           // The minimum number of SSTables to keep.  On compaction, we will always keep this number of SSTables instead of one large SSTable.
+	memtable           *avl.AVLTree   // The memtable is an in-memory AVL tree.
+	memtableSize       atomic.Int64   // The size of the memtable.
+	memtableLock       *sync.RWMutex  // Lock for the memtable.
+	sstables           []*SSTable     // The list of current SSTables.
+	sstablesLock       *sync.RWMutex  // Lock for the list of SSTables.
+	directory          string         // The directory where the SSTables are stored.
+	memtableFlushSize  int            // The size at which the memtable should be flushed to disk.
+	compactionInterval int            // The interval at which the LSM-tree should be compacted. (in number of SSTables)
+	minimumSSTables    int            // The minimum number of SSTables to keep.  On compaction, we will always keep this number of SSTables instead of one large SSTable.
+	activeTransactions []*Transaction // List of active transactions
+	wal                *Wal
+}
+
+// Wal is a struct representing a write-ahead log.
+type Wal struct {
+	file    *os.File
+	encoder *gob.Encoder
+	decoder *gob.Decoder
+	lock    *sync.RWMutex
 }
 
 // SSTable is a struct representing a sorted string table.
 type SSTable struct {
-	file   *os.File      // The opened SSTable file.
-	minKey []byte        // The minimum key in the SSTable.
-	maxKey []byte        // The maximum key in the SSTable.
-	lock   *sync.RWMutex // Lock for the SSTable.
+	file    *os.File      // The opened SSTable file.
+	encoder *gob.Encoder  // The encoder for the SSTable file.
+	minKey  []byte        // The minimum key in the SSTable.
+	maxKey  []byte        // The maximum key in the SSTable.
+	lock    *sync.RWMutex // Lock for the SSTable.
+}
+
+// OperationType is an enum representing the type of operation.
+type OperationType int
+
+const (
+	OpPut OperationType = iota
+	OpDelete
+)
+
+// Operation is a struct representing an operation in a transaction.
+type Operation struct {
+	Type  OperationType
+	Key   []byte // The key of the operation.
+	Value []byte // Only used for OpPut
+}
+
+// Transaction is a struct representing a transaction.
+type Transaction struct {
+	Operations []Operation // List of operations in the transaction.
+	Aborted    bool        // Whether the transaction has been aborted.
+}
+
+// SSTableIterator is an iterator for SSTable.
+type SSTableIterator struct {
+	file *os.File
+	dec  *gob.Decoder
 }
 
 // New creates a new LSM-tree or opens an existing one.
@@ -66,6 +105,12 @@ func New(directory string, directoryPerm os.FileMode, memtableFlushSize, compact
 			return nil, err
 		}
 
+		// Create the write-ahead log
+		walFile, err := os.OpenFile(fmt.Sprintf("%s%s%s", directory, string(os.PathSeparator), "wal"+WAL_EXTENSION), os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			return nil, err
+		}
+
 		return &LSMT{
 			memtable:           avl.NewAVLTree(),
 			memtableLock:       &sync.RWMutex{},
@@ -75,6 +120,7 @@ func New(directory string, directoryPerm os.FileMode, memtableFlushSize, compact
 			memtableFlushSize:  memtableFlushSize,
 			compactionInterval: compactionInterval,
 			minimumSSTables:    minimumSSTables,
+			wal:                &Wal{file: walFile, encoder: gob.NewEncoder(walFile), decoder: gob.NewDecoder(walFile), lock: &sync.RWMutex{}},
 		}, nil
 	} else {
 
@@ -130,6 +176,71 @@ func New(directory string, directoryPerm os.FileMode, memtableFlushSize, compact
 
 }
 
+// WriteOperation writes an operation to the write-ahead log.
+func (wal *Wal) WriteOperation(op Operation) error {
+	wal.lock.Lock()
+	defer wal.lock.Unlock()
+
+	err := wal.encoder.Encode(op)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Recover reads the write-ahead log and recovers the operations.
+func (wal *Wal) Recover() ([]Operation, error) {
+	wal.lock.Lock()
+	defer wal.lock.Unlock()
+
+	var operations []Operation
+
+	for {
+		var op Operation
+		err := wal.decoder.Decode(&op)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		operations = append(operations, op)
+	}
+
+	return operations, nil
+}
+
+// RunRecoveredOperations runs the recovered operations from the write-ahead log.
+func (l *LSMT) RunRecoveredOperations(operations []Operation) error {
+	for _, op := range operations {
+		switch op.Type {
+		case OpPut:
+			err := l.Put(op.Key, op.Value)
+			if err != nil {
+				return err
+			}
+		case OpDelete:
+			err := l.Delete(op.Key)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// Next returns the next key-value pair from the SSTable.
+func (it *SSTableIterator) Next() (*KeyValue, error) {
+	var kv KeyValue
+	err := it.dec.Decode(&kv)
+	if err != nil {
+		return nil, err
+	}
+	return &kv, nil
+}
+
 // Put inserts a key-value pair into the LSM-tree.
 func (l *LSMT) Put(key, value []byte) error {
 	// We will first put the key-value pair in the memtable.
@@ -149,7 +260,6 @@ func (l *LSMT) Put(key, value []byte) error {
 
 	// If the memtable size exceeds the flush size, flush the memtable to disk.
 	if l.memtableSize.Load() > int64(l.memtableFlushSize) {
-		log.Println("Flushing memtable...")
 		if err := l.flushMemtable(); err != nil {
 			return err
 		}
@@ -158,6 +268,18 @@ func (l *LSMT) Put(key, value []byte) error {
 	}
 
 	return nil
+}
+
+// getSSTableIterator returns an iterator for the SSTable.
+func getSSTableIterator(file *os.File) (*SSTableIterator, error) {
+	_, err := file.Seek(0, 0)
+	if err != nil {
+		return nil, err
+	}
+	return &SSTableIterator{
+		file: file,
+		dec:  gob.NewDecoder(file),
+	}, nil
 }
 
 // flushMemtable flushes the memtable to disk, creating a new SSTable.
@@ -184,7 +306,6 @@ func (l *LSMT) flushMemtable() error {
 
 	// Check the amount of sstables and if we need to compact
 	if len(l.sstables) > l.compactionInterval {
-		log.Println("Compacting LSM-tree...")
 		if err := l.Compact(); err != nil {
 			return err
 		}
@@ -226,33 +347,20 @@ func (l *LSMT) newSSTable(directory string, memtable *avl.AVLTree) (*SSTable, er
 
 	enc := gob.NewEncoder(ssltableFile)
 
-	enc.Encode(sstableSlice)
+	for _, kv := range sstableSlice {
+		err = enc.Encode(kv)
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	return &SSTable{
-		file:   ssltableFile,
-		minKey: sstableSlice[0].Key,
-		maxKey: sstableSlice[len(sstableSlice)-1].Key,
+		file:    ssltableFile,
+		minKey:  sstableSlice[0].Key,
+		maxKey:  sstableSlice[len(sstableSlice)-1].Key,
+		lock:    &sync.RWMutex{},
+		encoder: enc,
 	}, nil
-}
-
-// getSSTableKVs reads the key-value pairs from the SSTable file.
-func getSSTableKVs(file *os.File) ([]*KeyValue, error) {
-	// Seek to start of file
-	_, err := file.Seek(0, 0)
-	if err != nil {
-		return nil, err
-	}
-
-	// Decode the SSTable file.
-	dec := gob.NewDecoder(file)
-
-	var kvs []*KeyValue
-	err = dec.Decode(&kvs)
-	if err != nil {
-		return nil, err
-	}
-
-	return kvs, nil
 }
 
 // Get retrieves the value for a given key from the LSM-tree.
@@ -289,16 +397,24 @@ func (l *LSMT) Get(key []byte) ([]byte, error) {
 			continue
 		}
 
-		// Read the key-value pairs from the SSTable file.
-		kvs, err := getSSTableKVs(sstable.file)
+		// Get an iterator for the SSTable file.
+		it, err := getSSTableIterator(sstable.file)
 		if err != nil {
 			return nil, err
 		}
 
-		// Perform a binary search on the SSTable.
-		index := binarySearch(kvs, key)
-		if index != -1 {
-			return kvs[index].Value, nil
+		// Iterate over the SSTable.
+		for {
+			kv, err := it.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return nil, err
+			}
+
+			if bytes.Compare(kv.Key, key) == 0 {
+				return kv.Value, nil
+			}
 		}
 	}
 
@@ -346,21 +462,45 @@ func (l *LSMT) Compact() error {
 	// Iterate over all existing SSTables.
 	for _, sstable := range l.sstables {
 		// Read all key-value pairs from the SSTable.
-		kvs, err := getSSTableKVs(sstable.file)
+
+		// Get an iterator for the SSTable file.
+		it, err := getSSTableIterator(sstable.file)
 		if err != nil {
 			return err
 		}
 
-		// For each key-value pair, check if the value is a tombstone.
-		for _, kv := range kvs {
+		// Iterate over the SSTable.
+		for {
+			kv, err := it.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return err
+			}
+
 			if bytes.Compare(kv.Value, []byte(TOMBSTONE_VALUE)) == 0 { // If the value is a tombstone, skip this key-value pair
 				continue
 			}
 
 			// If the value is not a tombstone, add it to the new memtable.
 			newMemtable.Insert(kv.Key, kv.Value)
-
 		}
+
+		//kvs, err := getSSTableKVs(sstable.file)
+		//if err != nil {
+		//	return err
+		//}
+		//
+		//// For each key-value pair, check if the value is a tombstone.
+		//for _, kv := range kvs {
+		//	if bytes.Compare(kv.Value, []byte(TOMBSTONE_VALUE)) == 0 { // If the value is a tombstone, skip this key-value pair
+		//		continue
+		//	}
+		//
+		//	// If the value is not a tombstone, add it to the new memtable.
+		//	newMemtable.Insert(kv.Key, kv.Value)
+		//
+		//}
 
 		sstable.file.Close() // Close the SSTable file.
 
@@ -411,15 +551,24 @@ func (l *LSMT) Compact() error {
 
 // Close closes the LSM-tree gracefully closing all opened SSTable files.
 func (l *LSMT) Close() error {
-	// Flush the memtable to disk.
-	if err := l.flushMemtable(); err != nil {
-		return err
+	// Check size of memtable
+	if l.memtableSize.Load() > 0 {
+
+		// Flush the memtable to disk.
+		if err := l.flushMemtable(); err != nil {
+			return err
+		}
 	}
 
-	// Close all SSTable files.
-	for _, sstable := range l.sstables {
-		if err := sstable.file.Close(); err != nil {
-			return err
+	if len(l.sstables) > 0 {
+		// Close all SSTable files.
+		for _, sstable := range l.sstables {
+			if sstable.file != nil {
+				if err := sstable.file.Close(); err != nil {
+					return err
+				}
+			}
+
 		}
 	}
 
@@ -428,20 +577,6 @@ func (l *LSMT) Close() error {
 
 // SplitSSTable splits a compacted SSTable into n smaller SSTables.
 func (l *LSMT) SplitSSTable(sstable *SSTable, n int) ([]*SSTable, error) {
-	// Read all key-value pairs from the SSTable.
-	kvs, err := getSSTableKVs(sstable.file)
-	if err != nil {
-		return nil, err
-	}
-
-	// Close the SSTable file.
-	sstable.file.Close()
-
-	// delete the sstable file
-	err = os.Remove(sstable.file.Name())
-	if err != nil {
-		return nil, err
-	}
 
 	memTables := make([]*avl.AVLTree, n)
 
@@ -451,12 +586,45 @@ func (l *LSMT) SplitSSTable(sstable *SSTable, n int) ([]*SSTable, error) {
 
 	memtSeq := 0
 
-	for _, kv := range kvs {
-		memTables[memtSeq].Insert(kv.Key, kv.Value)
-		memtSeq++
-		if memtSeq == n {
-			memtSeq = 0
+	// Get an iterator for the SSTable file.
+	it, err := getSSTableIterator(sstable.file)
+	if err != nil {
+		return nil, err
+	}
+
+	// Iterate over the SSTable.
+	for {
+		kv, err := it.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
 		}
+
+		// If the value is a tombstone, skip this key-value pair.
+		if bytes.Compare(kv.Value, []byte(TOMBSTONE_VALUE)) == 0 {
+			continue
+		}
+
+		// If the value is not a tombstone, add it to the memtable.
+		if memtSeq < len(memTables) {
+			memTables[memtSeq].Insert(kv.Key, kv.Value)
+
+			// If we have reached the size of the memtable, flush it to disk.
+			if memTables[memtSeq].GetSize() >= l.memtableFlushSize {
+				memtSeq++
+			}
+		}
+
+	}
+
+	// Close the SSTable file.
+	sstable.file.Close()
+
+	// delete the sstable file
+	err = os.Remove(sstable.file.Name())
+	if err != nil {
+		return nil, err
 	}
 
 	sstables := make([]*SSTable, n)
@@ -511,18 +679,26 @@ func (l *LSMT) Range(start, end []byte) ([][]byte, [][]byte, error) {
 			continue
 		}
 
-		// Read the key-value pairs from the SSTable file.
-		kvs, err := getSSTableKVs(sstable.file)
+		// Get an iterator for the SSTable file.
+		it, err := getSSTableIterator(sstable.file)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		// Perform a binary search on the SSTable.
-		for _, kv := range kvs {
+		// Iterate over the SSTable.
+		for {
+			kv, err := it.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return nil, nil, err
+			}
+
 			if bytes.Compare(kv.Key, start) >= 0 && bytes.Compare(kv.Key, end) <= 0 {
 				keys = append(keys, kv.Key)
 				values = append(values, kv.Value)
 			}
+
 		}
 	}
 
@@ -563,18 +739,26 @@ func (l *LSMT) NRange(start, end []byte) ([][]byte, [][]byte, error) {
 			continue
 		}
 
-		// Read the key-value pairs from the SSTable file.
-		kvs, err := getSSTableKVs(sstable.file)
+		// Get an iterator for the SSTable file.
+		it, err := getSSTableIterator(sstable.file)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		// Perform a binary search on the SSTable.
-		for _, kv := range kvs {
+		// Iterate over the SSTable.
+		for {
+			kv, err := it.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return nil, nil, err
+			}
+
 			if bytes.Compare(kv.Key, start) < 0 || bytes.Compare(kv.Key, end) > 0 {
 				keys = append(keys, kv.Key)
 				values = append(values, kv.Value)
 			}
+
 		}
 	}
 
@@ -611,18 +795,26 @@ func (l *LSMT) GreaterThan(key []byte) ([][]byte, [][]byte, error) {
 
 		sstable := l.sstables[i]
 
-		// Read the key-value pairs from the SSTable file.
-		kvs, err := getSSTableKVs(sstable.file)
+		// Get an iterator for the SSTable file.
+		it, err := getSSTableIterator(sstable.file)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		// Perform a binary search on the SSTable.
-		for _, kv := range kvs {
+		// Iterate over the SSTable.
+		for {
+			kv, err := it.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return nil, nil, err
+			}
+
 			if bytes.Compare(kv.Key, key) > 0 {
 				keys = append(keys, kv.Key)
 				values = append(values, kv.Value)
 			}
+
 		}
 	}
 
@@ -658,18 +850,26 @@ func (l *LSMT) GreaterThanEqual(key []byte) ([][]byte, [][]byte, error) {
 
 		sstable := l.sstables[i]
 
-		// Read the key-value pairs from the SSTable file.
-		kvs, err := getSSTableKVs(sstable.file)
+		// Get an iterator for the SSTable file.
+		it, err := getSSTableIterator(sstable.file)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		// Perform a binary search on the SSTable.
-		for _, kv := range kvs {
+		// Iterate over the SSTable.
+		for {
+			kv, err := it.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return nil, nil, err
+			}
+
 			if bytes.Compare(kv.Key, key) >= 0 {
 				keys = append(keys, kv.Key)
 				values = append(values, kv.Value)
 			}
+
 		}
 	}
 
@@ -705,18 +905,26 @@ func (l *LSMT) LessThan(key []byte) ([][]byte, [][]byte, error) {
 
 		sstable := l.sstables[i]
 
-		// Read the key-value pairs from the SSTable file.
-		kvs, err := getSSTableKVs(sstable.file)
+		// Get an iterator for the SSTable file.
+		it, err := getSSTableIterator(sstable.file)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		// Perform a binary search on the SSTable.
-		for _, kv := range kvs {
+		// Iterate over the SSTable.
+		for {
+			kv, err := it.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return nil, nil, err
+			}
+
 			if bytes.Compare(kv.Key, key) < 0 {
 				keys = append(keys, kv.Key)
 				values = append(values, kv.Value)
 			}
+
 		}
 	}
 
@@ -752,18 +960,26 @@ func (l *LSMT) LessThanEqual(key []byte) ([][]byte, [][]byte, error) {
 
 		sstable := l.sstables[i]
 
-		// Read the key-value pairs from the SSTable file.
-		kvs, err := getSSTableKVs(sstable.file)
+		// Get an iterator for the SSTable file.
+		it, err := getSSTableIterator(sstable.file)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		// Perform a binary search on the SSTable.
-		for _, kv := range kvs {
+		// Iterate over the SSTable.
+		for {
+			kv, err := it.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return nil, nil, err
+			}
+
 			if bytes.Compare(kv.Key, key) <= 0 {
 				keys = append(keys, kv.Key)
 				values = append(values, kv.Value)
 			}
+
 		}
 	}
 
@@ -799,14 +1015,21 @@ func (l *LSMT) NGet(key []byte) ([][]byte, [][]byte, error) {
 
 		sstable := l.sstables[i]
 
-		// Read the key-value pairs from the SSTable file.
-		kvs, err := getSSTableKVs(sstable.file)
+		// Get an iterator for the SSTable file.
+		it, err := getSSTableIterator(sstable.file)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		// Perform a binary search on the SSTable.
-		for _, kv := range kvs {
+		// Iterate over the SSTable.
+		for {
+			kv, err := it.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return nil, nil, err
+			}
+
 			if bytes.Compare(kv.Key, key) != 0 {
 				keys = append(keys, kv.Key)
 				values = append(values, kv.Value)
@@ -815,4 +1038,63 @@ func (l *LSMT) NGet(key []byte) ([][]byte, [][]byte, error) {
 	}
 
 	return keys, values, nil
+}
+
+// BeginTransaction starts a new transaction.
+func (l *LSMT) BeginTransaction() *Transaction {
+	tx := &Transaction{Operations: make([]Operation, 0), Aborted: false}
+	l.activeTransactions = append(l.activeTransactions, tx)
+	return tx
+}
+
+// AddPut adds a put operation to a transaction.
+func (tx *Transaction) AddPut(key, value []byte) {
+	tx.Operations = append(tx.Operations, Operation{Type: OpPut, Key: key, Value: value})
+}
+
+// AddDelete adds a delete operation to a transaction.
+func (tx *Transaction) AddDelete(key []byte) {
+	tx.Operations = append(tx.Operations, Operation{Type: OpDelete, Key: key})
+}
+
+// CommitTransaction commits a transaction.
+func (l *LSMT) CommitTransaction(tx *Transaction) error {
+	if tx.Aborted {
+		return errors.New("transaction has been aborted")
+	}
+
+	for _, op := range tx.Operations {
+		switch op.Type {
+		case OpPut:
+			if err := l.Put(op.Key, op.Value); err != nil {
+				return err
+			}
+		case OpDelete:
+			if err := l.Delete(op.Key); err != nil {
+				return err
+			}
+		}
+	}
+
+	// Remove the transaction from the active list.
+	for i, t := range l.activeTransactions {
+		if t == tx {
+			l.activeTransactions = append(l.activeTransactions[:i], l.activeTransactions[i+1:]...)
+			break
+		}
+	}
+
+	return nil
+}
+
+// RollbackTransaction aborts a transaction.
+func (l *LSMT) RollbackTransaction(tx *Transaction) {
+	tx.Aborted = true
+	// Optionally: Remove the transaction from active list
+	for i, t := range l.activeTransactions {
+		if t == tx {
+			l.activeTransactions = append(l.activeTransactions[:i], l.activeTransactions[i+1:]...)
+			break
+		}
+	}
 }
